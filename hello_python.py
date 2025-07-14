b986e33d743401ef0b2bf5694a79c355d6035bfc
olayade bukola <olayadebukola@gmail.com>
2025-07-14 17:13:34 +0100

    added first python script for AI Ops

---
+++ hello_python.py
@@ -0,0 +1,7 @@
+print("Welcome to Week 2 of AI Ops Bootcamp!")
+name = "AI Ops Engineer"
+print("I am learning to become an", name)
+number_of_weeks = 12
+weeks_completed = 1
+weeks_remaining = number_of_weeks - weeks_completed
+print("Weeks remaining in bootcamp:", weeks_remaining)
